0bee5ddf17b15f5f36029f3ebe0e487d7045b8fb
Nataliya Lapitskaya <75301381+LapitskayaN@users.noreply.github.com>
2022-08-17 12:25:19 +0300

    Update Module_7_CSV.py

--- Module_7_CSV.py
+++ Module_7_CSV.py
@@ -30,6 +30,11 @@ def list_of_letters(text=fileRead()):
     return [item for item in char_list if item.isalpha()]
 
 
+def list_of_lower_letters(text=fileRead()):
+    char_list = [char for char in text.lower()]
+    return [item for item in char_list if item.isalpha()]
+
+
 def wordsInLower(list_of_words=list_of_words()):
     words_lower_list = []
     for word in list_of_words:
@@ -59,38 +64,26 @@ def upperLowerLettersDict(letters_dict=countElements(list_of_letters())):
             lower_dict[key] = value
     return upper_dict, lower_dict
 
-'''
-print(list_of_words())
-print(list_of_letters())
-print(upperLowerLettersDict())
-'''
-
-def countLetterAppearance(upper=upperLowerLettersDict()[0], lower=upperLowerLettersDict()[1]):
-    count_all_dict = {}
-    for key, value in lower.items():
-        if key in upper.keys():
-            count_all_dict[key] = value + upper.get(key.upper())
-        else:
-            count_all_dict[key] = value
-    for key, value in upper.items():
-        if key.lower() in count_all_dict:
-            pass
-        else:
-            count_all_dict[key.lower] = value
-    return count_all_dict
-
 
 def totalLettersCount(letters=list_of_letters()):
     return len(letters)
 
-
-def csvFile1(total=totalLettersCount(), upper=upperLowerLettersDict()[0],
-                       count_letter_appearance=countLetterAppearance()):
+def csvFile1(count_words=None):
+    if count_words is None:
+        count_words = countElements(wordsInLower())
+    with open('../Python_DQE_Lapitskaya/task07_01.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter='-')
+        for key, value in count_words.items():
+            writer.writerow([key, value])
+            
+            
+def csvFile2(total=totalLettersCount(), upper=upperLowerLettersDict()[0],
+                       count_all_letter=countElements(list_of_lower_letters())):
     with open('../Python_DQE_Lapitskaya/task07_02.csv', 'w', newline='') as csv_file:
         header = ['letter', 'count_all', 'count_uppercase', 'percentage']
         writer = csv.DictWriter(csv_file, fieldnames=header)
         writer.writeheader()
-        for key, value in count_letter_appearance.items():
+        for key, value in count_all_letter.items():
             percent = round(value / total * 100, 2)
             upper_count = upper.get(key.upper())
             if upper_count is None:
@@ -99,16 +92,4 @@ def csvFile1(total=totalLettersCount(), upper=upperLowerLettersDict()[0],
                 {'letter': key, 'count_all': value, 'count_uppercase': upper_count, 'percentage': str(percent) + '%'})
             # print(key, value, upper_count, str(percent)+'%')
 
-
-def csvFile2(count_words=None):
-    if count_words is None:
-        count_words = countElements(wordsInLower())
-    with open('../Python_DQE_Lapitskaya/task07_01.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter='-')
-        for key, value in count_words.items():
-            writer.writerow([key, value])
-
-
-csvFile1()
-csvFile2()
 
